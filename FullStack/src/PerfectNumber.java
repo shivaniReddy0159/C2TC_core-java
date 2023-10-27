ca19757eaace1f360ba3f9cd511f679e4304c5e6
shiva <shiva@Shivamani>
2023-10-27 07:11:07 +0530

    third commit

--- FullStack/src/PerfectNumber.java
+++ FullStack/src/PerfectNumber.java
@@ -2,7 +2,25 @@
 public class PerfectNumber {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int num=6;
+		int sum=0;
+		int temp=sum;
+		for(int i=1;i<6;i++)
+		{
+			if(num%i==0)
+			{
+				sum=sum+i;
+			}
+		}
+		if(sum==num)
+		{
+			System.out.println("perfect number");
+		}
+		else
+		{
+			System.out.println("not a perfect number");
+		}
+
 
 	}
 
